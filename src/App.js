2e68b7bd7b0fe5eb16e5355b7ce57fbb573c3e5f
Andreas Daskalopoulos <andreas.daskalopoulos@citrix.com>
2019-02-21 13:06:07 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Productivity Analytics</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
