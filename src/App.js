bc190c36eb3376027cebd58bae4c6280e3b348a8
Andreas Daskalopoulos <andreas.daskalopoulos@citrix.com>
2019-02-21 17:49:47 +0200

    webpack 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+// import { Header, Icon } from 'rdx/semantic-ui-react'
 
 class App extends Component {
     render() {
         return (
             <div>
+                {/* <Header as='h2'>
+                    <Icon className="global access type size-48" />
+                    <Header.Content>
+                        Productivity Analytics
+                    </Header.Content>
+                </Header> */}
                 <h1>Productivity Analytics</h1>
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
-const path = require('path');
-const autoprefixer = require('autoprefixer');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
-    entry: './src/index.js',
+    mode: "development",
+    devtool: "cheap-module-eval-source-map",
+    entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        chunkFilename: '[id].js',
-        publicPath: ''
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        chunkFilename: "[id].js",
+        publicPath: ""
     },
     resolve: {
         extensions: ['.js', '.jsx']
@@ -18,26 +19,26 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
+                exclude: ['/node_modules'],
+                use: [{ loader: 'babel-loader' }],
             },
             {
                 test: /\.css$/,
                 exclude: /node_modules/,
                 use: [
-                    { loader: 'style-loader' },
+                    { loader: "style-loader" },
                     {
-                        loader: 'css-loader',
+                        loader: "css-loader",
                         options: {
                             importLoaders: 1,
                             modules: true,
-                            localIdentName: '[name]__[local]__[hash:base64:5]'
+                            localIdentName: "[name]__[local]__[hash:base64:5]"
                         }
                     },
                     {
-                        loader: 'postcss-loader',
+                        loader: "postcss-loader",
                         options: {
-                            ident: 'postcss',
+                            ident: "postcss",
                             plugins: () => [
                                 autoprefixer({
                                     browsers: [
@@ -52,15 +53,15 @@ module.exports = {
             },
             {
                 test: /\.(png|jpe?g|gif)$/,
-                loader: 'url-loader?limit=8000&name=images/[name].[ext]'
+                loader: "url-loader?limit=8000&name=images/[name].[ext]"
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: __dirname + '/src/index.html',
-            filename: 'index.html',
-            inject: 'body'
+            template: __dirname + "/src/index.html",
+            filename: "index.html",
+            inject: "body"
         })
     ]
 };
