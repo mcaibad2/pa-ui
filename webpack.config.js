987b493b69199a29ba431542198fb943eb43f81f
Andreas Daskalopoulos <andreas.daskalopoulos@citrix.com>
2019-02-25 12:45:42 +0200

    Followed steps at https://www.valentinog.com/blog/webpack-tutorial/

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,41 @@
-const path = require("path");
-const autoprefixer = require("autoprefixer");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    devtool: "cheap-module-eval-source-map",
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-        chunkFilename: "[id].js",
-        publicPath: ""
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: ['/node_modules'],
-                use: [{ loader: 'babel-loader' }],
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             },
             {
                 test: /\.css$/,
-                exclude: /node_modules/,
                 use: [
                     { loader: "style-loader" },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            importLoaders: 1,
-                            modules: true,
-                            localIdentName: "[name]__[local]__[hash:base64:5]"
-                        }
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            ident: "postcss",
-                            plugins: () => [
-                                autoprefixer({
-                                    browsers: [
-                                        "> 1%",
-                                        "last 2 versions"
-                                    ]
-                                })
-                            ]
-                        }
-                    }
+                    { loader: "css-loader" }
                 ]
             },
             {
-                test: /\.(png|jpe?g|gif)$/,
-                loader: "url-loader?limit=8000&name=images/[name].[ext]"
+                test: /\.html$/,
+                use: [{
+                    loader: "html-loader"
+                }]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {},
+                }]
             }
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + "/src/index.html",
-            filename: "index.html",
-            inject: "body"
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
         })
     ]
 };
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,70 +1,52 @@
-const ProgressBarPlugin = require("progress-bar-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const autoprefixer = require('autoprefixer');
 const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    mode: "production",
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
+        filename: "main.js",
         library: "pa",
         libraryTarget: "umd",
     },
     optimization: {
         minimizer: [new UglifyJsPlugin()]
     },
-    plugins: [
-        new ProgressBarPlugin(),
-        new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") })
-    ],
-    externals: [
-        "react",
-        "rdx",
-        { "rdx/semantic-ui-react": "rdxSemanticUIReact" },
-    ],
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: ['/node_modules'],
-                use: [{ loader: 'babel-loader' }],
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             },
             {
                 test: /\.css$/,
-                exclude: /node_modules/,
                 use: [
                     { loader: "style-loader" },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            importLoaders: 1,
-                            modules: true,
-                            localIdentName: "[name]__[local]__[hash:base64:5]"
-                        }
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            ident: "postcss",
-                            plugins: () => [
-                                autoprefixer({
-                                    browsers: [
-                                        "> 1%",
-                                        "last 2 versions"
-                                    ]
-                                })
-                            ]
-                        }
-                    }
+                    { loader: "css-loader" }
                 ]
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: "html-loader"
+                }]
             },
             {
                 test: /\.(png|jpe?g|gif)$/,
                 loader: "url-loader?limit=8000&name=images/[name].[ext]"
             }
         ]
-    }
+    },
+    externals: [
+        "react",
+        "rdx",
+        { "rdx/semantic-ui-react": "rdxSemanticUIReact" },
+    ],
+    plugins: [
+        new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") })
+    ]
 };
